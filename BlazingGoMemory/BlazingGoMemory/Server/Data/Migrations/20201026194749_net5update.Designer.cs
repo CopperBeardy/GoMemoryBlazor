2aa248d065b09bec9e7a5097243b1bbfb56b67f7
davin <copperbeard@protonamail.com>
2020-10-26 21:04:50 +0000

    Refactor Whatyousee page
    moved the game options settings into the Gameoptions Component
    moved the view data for memorisingscreen to a new MemorisingScreen Component
    change Gameoptions component and MemorisingGame component to use a CascadingValue for Game value.
    refactored the ToggleGameOptionsShowChanged method to use a bool instead of a ModeDifficulty value

--- BlazingGoMemory/BlazingGoMemory/Server/Data/Migrations/20201026194749_net5update.Designer.cs
+++ BlazingGoMemory/BlazingGoMemory/Server/Data/Migrations/20201026194749_net5update.Designer.cs
@@ -4,14 +4,16 @@ using BlazingGoMemory.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BlazingGoMemory.Server.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201026194749_net5update")]
+    partial class net5update
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
